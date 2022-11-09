7309e253e16ed9a07e79acbb4cfc84c05b4f4ef2
eddyhakimi <eddi.hakimi@gmx.de>
2022-11-09 20:55:39 +0100

    Add mockup data
    Signed-off-by: eddyhakimi <eddi.hakimi@gmx.de>

---
+++ src/deskstar-frontend/bookings.js
@@ -0,0 +1,51 @@
+export const bookings = [
+    {
+        userId: "1",
+        deskId: "1",
+        timestamp: "2022-10-08T12:00:00Z",
+        startTime: "2022-10-09T12:00:00Z",
+        endTime: "2022-10-09T16:00:00Z"
+    },
+    {
+        userId: "1",
+        deskId: "1",
+        timestamp: "2022-10-09T16:01:00Z",
+        startTime: "2022-10-10T12:00:00Z",
+        endTime: "2022-10-10T16:00:00Z"
+    },
+    {
+        userId: "1",
+        deskId: "1",
+        timestamp: "2022-10-10T16:01:00Z",
+        startTime: "2022-10-11T12:00:00Z",
+        endTime: "2022-10-11T16:00:00Z"
+    },
+    {
+        userId: "1",
+        deskId: "3",
+        timestamp: "2022-10-12T12:00:00Z",
+        startTime: "2022-10-12T19:00:00Z",
+        endTime: "2022-10-12T23:00:00Z"
+    },
+    {
+        userId: "1",
+        deskId: "9",
+        timestamp: "2022-10-12T23:10:00Z",
+        startTime: "2022-10-13T12:00:00Z",
+        endTime: "2022-10-13T16:00:00Z"
+    },
+    {
+        userId: "1",
+        deskId: "12",
+        timestamp: "2022-10-14T08:00:00Z",
+        startTime: "2022-10-19T14:00:00Z",
+        endTime: "2022-10-19T18:00:00Z"
+    },
+    {
+        userId: "1",
+        deskId: "21",
+        timestamp: "2022-10-23T08:00:00Z",
+        startTime: "2022-10-23T09:00:00Z",
+        endTime: "2022-10-23T16:00:00Z"
+    },
+];
