a28a987aa2dbfde8493d3e59d052f9a4df1cb920
Fiona <fiona.sternberg@fau.de>
2022-11-12 16:18:16 +0100

    build first structure
    Co-authored-by: xilef45 <felix.l.lang@fau.de>

---
+++ src/deskstar-backend/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Deskstar.Usecases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deskstar.Controllers;
+
+[ApiController]
+[Route("/bookings")]
+public class BookingController : ControllerBase
+{
+    private readonly ILogger<BookingController> _logger;
+    private readonly IBookingUsecases _bookingUsecases;
+    private readonly IConfiguration _configuration;
+
+
+    public BookingController(ILogger<BookingController> logger, IBookingUsecases bookingUsecases, IConfiguration configuration)
+    {
+        _logger = logger;
+        _bookingUsecases = bookingUsecases;
+        _configuration = configuration;
+    }
+    
+    [HttpGet("recent")]
+    [Authorize]
+    public IActionResult RecentBookings()
+    {
+        return Ok();
+    }
+}
---
+++ src/deskstar-backend/Usecases/BookingUsecases.cs
@@ -0,0 +1,25 @@
+﻿using Deskstar.DataAccess;
+using Deskstar.Entities;
+
+namespace Deskstar.Usecases;
+
+public interface IBookingUsecases
+{
+    public List<Booking> GetRecentBookings();
+}
+public class BookingUsecases : IBookingUsecases
+{
+    private readonly ILogger<BookingUsecases> _logger;
+    private readonly DataContext _context;
+    
+    public BookingUsecases(ILogger<BookingUsecases> logger, DataContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public List<Booking> GetRecentBookings()
+    {
+        return null;
+    }
+}
