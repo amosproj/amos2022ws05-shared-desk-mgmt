559cb9fcd728bc7d42f992c1ec9e02705bc5f88f
xilef45 <felix.l.lang@fau.de>
2022-11-15 13:32:56 +0100

    updated RecentBooking.cs and prepared for better error messages
    Co-authored-by: Fiona <fiona.sternberg@fau.de>

--- src/deskstar-backend/Deskstar/Controllers/BookingController.cs
+++ src/deskstar-backend/Deskstar/Controllers/BookingController.cs
@@ -11,18 +11,13 @@ namespace Deskstar.Controllers;
 [Route("/bookings")]
 public class BookingController : ControllerBase
 {
-    private readonly IAuthUsecases _authUsecases;
     private readonly IBookingUsecases _bookingUsecases;
-    private readonly IConfiguration _configuration;
     private readonly ILogger<BookingController> _logger;
 
-    public BookingController(ILogger<BookingController> logger, IBookingUsecases bookingUsecases,
-        IConfiguration configuration, IAuthUsecases authUsecases)
+    public BookingController(ILogger<BookingController> logger, IBookingUsecases bookingUsecases)
     {
         _logger = logger;
         _bookingUsecases = bookingUsecases;
-        _configuration = configuration;
-        _authUsecases = authUsecases;
     }
 
     [HttpGet("recent")]
@@ -34,6 +29,16 @@ public class BookingController : ControllerBase
         var jwtSecurityToken = handler.ReadJwtToken(accessToken);
         // TODO get user id when it's in token
         var mailAddress = jwtSecurityToken.Claims.First(claim => claim.Type == JwtRegisteredClaimNames.Email).Value;
-        return Ok(JsonSerializer.Serialize(_bookingUsecases.GetRecentBookings(mailAddress)));
+        try
+        {
+            var bookings = _bookingUsecases.GetRecentBookings(mailAddress);
+            return Ok(JsonSerializer.Serialize(bookings));
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+        }
+
+        return null;
     }
 }
--- src/deskstar-backend/Deskstar/Usecases/BookingUsecases.cs
+++ src/deskstar-backend/Deskstar/Usecases/BookingUsecases.cs
@@ -23,25 +23,22 @@ public class BookingUsecases : IBookingUsecases
     public List<RecentBooking> GetRecentBookings(string mailAddress)
     {
         var userId = _getUser(mailAddress).UserId;
-        try
+        var bookings = _context.Bookings
+            .Where(b => b.UserId == userId && b.StartTime >= DateTime.Now)
+            .OrderBy(b => b.StartTime)
+            .Take(10);
+        var mapBookingsToRecentBookings = bookings.Select(b => new RecentBooking
         {
-            var bookings = _context.Bookings.Where(b => b.UserId == userId);
-            var mapBookingsToRecentBookings = bookings.Select(b => new RecentBooking
-            {
-                DeskName = b.Desk.DeskName,
-                EndTime = b.EndTime,
-                StartTime = b.StartTime,
-                Timestamp = b.Timestamp
-            });
-
-            return mapBookingsToRecentBookings.ToList();
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, e.Message);
-        }
-
-        return new List<RecentBooking>();
+            DeskName = b.Desk.DeskName,
+            EndTime = b.EndTime,
+            StartTime = b.StartTime,
+            Timestamp = b.Timestamp,
+            BuildingName = b.Desk.Room.Floor.Building.BuildingName,
+            FloorName = b.Desk.Room.Floor.FloorName,
+            RoomName = b.Desk.Room.RoomName
+        });
+        
+        return mapBookingsToRecentBookings.ToList();
     }
 
     private User _getUser(string mail)
