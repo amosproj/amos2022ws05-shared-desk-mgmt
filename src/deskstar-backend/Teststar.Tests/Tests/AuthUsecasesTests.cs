1141a09126ddfbc8668a935484f9e7102fd05110
xilef45 <88111260+xilef45@users.noreply.github.com>
2022-11-16 09:08:39 +0100

    Update AuthUsecasesTests.cs

--- src/deskstar-backend/Teststar.Tests/Tests/AuthUsecasesTests.cs
+++ src/deskstar-backend/Teststar.Tests/Tests/AuthUsecasesTests.cs
@@ -9,151 +9,145 @@ using Moq;
 
 namespace Teststar.Tests.Tests;
 
-public class AuthUsecasesTests
+public class AuthUseCasesTests
 {
     [Test]
     public void CheckCredentials_ValidMailAndPassword()
     {
         //setup
-        using (var mogDB = new DataContext())
-        {
-            AddOneCompany_AddOneUser(mogDB, new PasswordHasher<User>());
+        using var mogDB = new DataContext();
+        AddOneCompany_AddOneUser(mogDB, new PasswordHasher<User>());
 
-            //arrange
-            var logger = new Mock<ILogger<AuthUsecases>>();
-            var subject = new AuthUsecases(logger.Object, mogDB);
+        //arrange
+        var logger = new Mock<ILogger<AuthUsecases>>();
+        var subject = new AuthUsecases(logger.Object, mogDB);
 
-            var mail = "test@mail.de";
-            var pw = "testpw";
+        const string mail = "test@mail.de";
+        const string pw = "testpw";
 
-            //act
-            var result = subject.checkCredentials(mail, pw);
+        //act
+        var result = subject.checkCredentials(mail, pw);
 
 
-            //assert
-            Assert.That(result);
-        }
+        //assert
+        Assert.That(result);
     }
 
     [Test]
     public void CreateToken_ValidMailAndPassword()
     {
-        using (var mogDB = new DataContext())
+        using var mogDB = new DataContext();
+        AddOneCompany_AddOneUser(mogDB, new PasswordHasher<User>());
+
+        //arrange
+        var logger = new Mock<ILogger<AuthUsecases>>();
+        var subject = new AuthUsecases(logger.Object, mogDB);
+        var mogSettings = new Dictionary<string, string>
         {
-            AddOneCompany_AddOneUser(mogDB, new PasswordHasher<User>());
-
-            //arrange
-            var logger = new Mock<ILogger<AuthUsecases>>();
-            var subject = new AuthUsecases(logger.Object, mogDB);
-            var mogSettings = new Dictionary<string, string>
-            {
-                { "Jwt:Issuer", "https://deskstar.com/" },
-                { "Jwt:Audience", "https://deskstar.com/" },
-                { "Jwt:Key", "thisIsATopSecretForTesting" }
-            };
-            var configuration = new ConfigurationBuilder().AddInMemoryCollection(mogSettings).Build();
+            { "Jwt:Issuer", "https://deskstar.com/" },
+            { "Jwt:Audience", "https://deskstar.com/" },
+            { "Jwt:Key", "thisIsATopSecretForTesting" }
+        };
+        var configuration = new ConfigurationBuilder().AddInMemoryCollection(mogSettings).Build();
 
 
-            var mail = "test@mail.de";
+        const string mail = "test@mail.de";
 
-            //act
-            var result = subject.createToken(configuration, mail);
+        //act
+        var result = subject.createToken(configuration, mail);
 
 
-            //assert
-            Assert.That(result, Is.Not.Null);
-        }
+        //assert
+        Assert.That(result, Is.Not.Null);
     }
 
     [Test]
     public void RegisterUser_NewUser()
     {
-        using (var mogDB = new DataContext())
+        using var mogDB = new DataContext();
+        var company = new Company
         {
-            var company = new Company
-            {
-                CompanyId = new Guid(),
-                CompanyName = "gehmalbierholn"
-            };
+            CompanyId = new Guid(),
+            CompanyName = "gehmalbierholn"
+        };
 
-            mogDB.Companies.Add(company);
-            mogDB.SaveChanges();
+        mogDB.Companies.Add(company);
+        mogDB.SaveChanges();
 
-            //arrange
-            var logger = new Mock<ILogger<AuthUsecases>>();
-            var subject = new AuthUsecases(logger.Object, mogDB);
+        //arrange
+        var logger = new Mock<ILogger<AuthUsecases>>();
+        var subject = new AuthUsecases(logger.Object, mogDB);
            
-            var user = new RegisterUser();
-            user.MailAddress = "test@mail.de";
-            user.FirstName = "testF";
-            user.LastName = "testL";
-            user.Password = "testpw";
-            user.CompanyId = company.CompanyId;
+        var user = new RegisterUser();
+        user.MailAddress = "test@mail.de";
+        user.FirstName = "testF";
+        user.LastName = "testL";
+        user.Password = "testpw";
+        user.CompanyId = company.CompanyId;
 
-            //act
-            var result = subject.registerUser(user);
+        //act
+        var result = subject.registerUser(user);
 
-            //assert
-            Assert.That(result);
-        }
+        //assert
+        Assert.That(result, Is.EqualTo(RegisterReturn.Ok));
     }
 
     [Test]
     public void RegisterUser_ExistingMail()
     {
-        using (var mogDB = new DataContext())
+        using var mogDB = new DataContext();
+        AddOneCompany_AddOneUser(mogDB, new PasswordHasher<User>());
+
+        //arrange
+        var logger = new Mock<ILogger<AuthUsecases>>();
+        var subject = new AuthUsecases(logger.Object, mogDB);
+        var hasher = new PasswordHasher<User>();
+
+        var user = new RegisterUser
         {
-            AddOneCompany_AddOneUser(mogDB, new PasswordHasher<User>());
+            MailAddress = "test@mail.de",
+            FirstName = "testF",
+            LastName = "testL",
+            Password = "password"
+        };
 
-            //arrange
-            var logger = new Mock<ILogger<AuthUsecases>>();
-            var subject = new AuthUsecases(logger.Object, mogDB);
-            var hasher = new PasswordHasher<User>();
+        //act
+        var result = subject.registerUser(user);
 
-            var user = new RegisterUser();
-            user.MailAddress = "test@mail.de";
-            user.FirstName = "testF";
-            user.LastName = "testL";
-            user.Password = "password";
-
-            //act
-            var result = subject.registerUser(user);
-
-            //assert
-            Assert.That(result, Is.False);
-        }
+        //assert
+        Assert.That(result, Is.False);
     }
 
     [Test]
     public void RegisterUser_NonExistingCompany()
     {
-        using (var mogDB = new DataContext())
+        using var mogDB = new DataContext();
+        var company = new Company
         {
-            var company = new Company
-            {
-                CompanyId = new Guid(),
-                CompanyName = "gehmalbierholn"
-            };
+            CompanyId = new Guid(),
+            CompanyName = "gehmalbierholn"
+        };
 
-            mogDB.Companies.Add(company);
-            mogDB.SaveChanges();
+        mogDB.Companies.Add(company);
+        mogDB.SaveChanges();
 
-            //arrange
-            var logger = new Mock<ILogger<AuthUsecases>>();
-            var subject = new AuthUsecases(logger.Object, mogDB);
+        //arrange
+        var logger = new Mock<ILogger<AuthUsecases>>();
+        var subject = new AuthUsecases(logger.Object, mogDB);
 
-            var user = new RegisterUser();
-            user.MailAddress = "test@mail.de";
-            user.FirstName = "testF";
-            user.LastName = "testL";
-            user.Password = "testpw";
+        var user = new RegisterUser
+        {
+            MailAddress = "test@mail.de",
+            FirstName = "testF",
+            LastName = "testL",
+            Password = "testpw"
+        };
 
-            //act
-            var result = subject.registerUser(user);
+        //act
+        var result = subject.registerUser(user);
 
-            //assert
-            Assert.That(result, Is.False);
-        }
+        //assert
+        Assert.That(result, Is.False);
     }
 
     private void AddOneCompany_AddOneUser(DataContext mogDB, PasswordHasher<User> hasher)
