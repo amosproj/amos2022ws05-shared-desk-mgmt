f7dc0debdd5a1989424aa27027212c49c011820c
Fiona <55480320+Faoilthiama@users.noreply.github.com>
2022-11-20 15:25:21 +0100

    Update Program.cs
    Signed-off-by: Fiona <55480320+Faoilthiama@users.noreply.github.com>

--- src/deskstar-backend/Deskstar/Program.cs
+++ src/deskstar-backend/Deskstar/Program.cs
@@ -38,7 +38,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(c => c.SchemaFilter<EnumSchemaFilter>());
 
 builder.Configuration.AddEnvironmentVariables();
