b266c4df3215f51564275aeb5f6b3ca8fb1ef73a
eddyhakimi <eddi.hakimi@gmx.de>
2022-11-20 17:28:45 +0000

    Display upcoming bookings & user options
    Signed-off-by: eddyhakimi <eddi.hakimi@gmx.de>

--- src/deskstar-frontend/pages/bookings.tsx
+++ src/deskstar-frontend/pages/bookings.tsx
@@ -1,12 +1,38 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+import BookingsTable from "../components/BookingsTable";
+import { bookings } from "../bookings";
+import { IBooking } from "../types/booking";
 
 export default function Bookings() {
+  const onDelete = (booking: IBooking) => {
+    //TODO: implement
+    console.log(`Pressed delete on ${booking.bookingId}`);
+  };
+  const onEdit = (booking: IBooking) => {
+    //TODO: implement
+    console.log(`Pressed edit on ${booking.bookingId}`);
+  };
+
   return (
     <div>
       <Head>
         <title>Bookings</title>
       </Head>
-      <h1 className="text-3xl font-bold">Bookings</h1>
+      <h1 className="text-3xl font-bold text-center my-10">My Bookings</h1>
+      <BookingsTable bookings={bookings} onEdit={onEdit} onDelete={onDelete} />
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  //TODO: fetch here upcoming bookings from backend
+  const sortedBookings = bookings.sort((a: IBooking, b: IBooking) =>
+    a.startTime.localeCompare(b.startTime)
+  );
+  return {
+    props: {
+      bookings: sortedBookings,
+    },
+  };
+};
