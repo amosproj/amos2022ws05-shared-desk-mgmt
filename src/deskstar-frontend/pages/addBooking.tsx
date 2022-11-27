a3a9501dfa837b08dcabcc7633aff60d7038205f
xilef45 <88111260+xilef45@users.noreply.github.com>
2022-11-27 15:02:01 +0100

    styling button
    Co-Authored-By: Fiona <55480320+Faoilthiama@users.noreply.github.com>

--- src/deskstar-frontend/pages/addBooking.tsx
+++ src/deskstar-frontend/pages/addBooking.tsx
@@ -151,7 +151,7 @@ const Bookings = ({ results }: { results: IRoom[] }) => {
         </div>
       </div>
 
-      <button type="button" onClick={onClick}>
+      <button type="button" className="btn btn-secondary" onClick={onClick}>
         Search for Desks
       </button>
     </div>
