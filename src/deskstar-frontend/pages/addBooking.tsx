13f03366bba741e63bb92d7f39892dd7ee13028d
xilef45 <88111260+xilef45@users.noreply.github.com>
2022-11-27 14:50:51 +0100

    Add dropdown buttons
    Co-Authored-By: Fiona <55480320+Faoilthiama@users.noreply.github.com>

--- src/deskstar-frontend/pages/addBooking.tsx
+++ src/deskstar-frontend/pages/addBooking.tsx
@@ -6,29 +6,23 @@ import { IRoom } from "../types/room";
 //TODO: delete this - just used for mockup data
 import { GetServerSideProps } from "next";
 import { rooms } from "../rooms";
+import build from "next/dist/build";
 
+const Bookings = ({ results }: { results: IRoom[] }) => {
+  const username = "Test User";
+  let buildings: string[] = [];
+  let locations: string[] = [];
+  let rooms: string[] = [];
+  let chosenBuildings: string[] = [];
+  let chosenLocations: string[] = [];
+  let chosenRooms: string[] = [];
 
+  for (const result of results) {
+    buildings.push(result.building);
+    locations.push(result.location);
+    rooms.push(result.roomName);
+  }
 
-const Bookings = ({results} : {results: IRoom[]}) => {
-    const username = 'Test User';
-
-    // TODO: Edit this to fit usecase
-    let formattedResults: any = {};
-    for (const result of results) {
-      if (!formattedResults[result.location])
-        formattedResults[result.location] = {};
-
-      if (!formattedResults[result.location][result.building])
-        formattedResults[result.location][result.building] = {};
-
-      if (!formattedResults[result.location][result.building][result])
-        formattedResults[result.location][result.building][result] = [];
-
-      formattedResults[result.location][result.building][result].push(
-        result
-      );
-    }
-    
   return (
     <div>
       <Head>
@@ -36,17 +30,133 @@ const Bookings = ({results} : {results: IRoom[]}) => {
       </Head>
       <h1 className="text-3xl font-bold text-center my-10">Add New Booking</h1>
       <div>Hello {username}, book your personal desk.</div>
-      <Collapse key="buildings" index={0} title="Buildings">
-        {Object.keys(results).map(
-          (building : string, buildingIndex: number) => (
-            <div key={buildingIndex}>{results[building]}</div>
-          )
-        )}
-      </Collapse>
-      <button type="button" onClick={onClick}>Search for Desks</button>
+
+      <div className="dropdown dropdown-hover">
+        <label tabIndex={0} className="btn m-1">
+          Locations
+        </label>
+        <div className="form-control">
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li key="allLocations">
+              <label className="label cursor-pointer">
+                <span className="label-text">All Locations</span>
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  onClick={() => {
+                    locations.forEach((location) => {
+                      //ToDO: check all Boxes
+                      chosenLocations.push(location);
+                    });
+                  }}
+                />
+              </label>
+            </li>
+            {locations.map((location: string) => (
+              <li key={"li_" + location}>
+                <label className="label cursor-pointer">
+                  <span className="label-text">{location}</span>
+                  <input
+                    key={location}
+                    type="checkbox"
+                    className="checkbox"
+                    onClick={() => {
+                      if (chosenLocations.includes(location)) {
+                        const index = chosenLocations.indexOf(location, 0);
+                        if (index > -1) {
+                          chosenLocations.splice(index, 1);
+                        }
+                      } else {
+                        chosenLocations.push(location);
+                      }
+                    }}
+                  />
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="dropdown dropdown-hover">
+        <label tabIndex={0} className="btn m-1">
+          Buildings
+        </label>
+        <div className="form-control">
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            //ToDO: add all check box
+          >
+            {buildings.map((building: string) => (
+              <li key={building}>
+                <label className="label cursor-pointer">
+                  <span className="label-text">{building}</span>
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    onClick={() => {
+                      if (chosenBuildings.includes(building)) {
+                        const index = chosenBuildings.indexOf(building, 0);
+                        if (index > -1) {
+                          chosenBuildings.splice(index, 1);
+                        }
+                      } else {
+                        chosenBuildings.push(building);
+                      }
+                    }}
+                  />
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="dropdown dropdown-hover">
+        <label tabIndex={0} className="btn m-1">
+          Rooms
+        </label>
+        <div className="form-control">
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            //ToDO: add all check box
+          >
+            {rooms.map((room: string) => (
+              <li key={room}>
+                <label className="label cursor-pointer">
+                  <span className="label-text">{room}</span>
+                  <input
+                    type="checkbox"
+                    className="checkbox"
+                    onClick={() => {
+                      if (chosenRooms.includes(room)) {
+                        const index = chosenRooms.indexOf(room, 0);
+                        if (index > -1) {
+                          chosenRooms.splice(index, 1);
+                        }
+                      } else {
+                        chosenRooms.push(room);
+                      }
+                    }}
+                  />
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <button type="button" onClick={onClick}>
+        Search for Desks
+      </button>
     </div>
   );
-}
+};
 
 //TODO: delete this - this is just for developing this component
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -57,8 +167,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
   };
 };
 
-function onClick() {
-
-}
+function onClick() {}
 
 export default Bookings;
