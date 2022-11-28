c74ce64d5267c67c6d852b98ada2efb131a7965f
eddyhakimi <eddi.hakimi@gmx.de>
2022-11-28 12:56:42 +0000

    Add users overview and user requests page
    Signed-off-by: eddyhakimi <eddi.hakimi@gmx.de>

---
+++ src/deskstar-frontend/pages/userRequests.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function UserRequests() {
+    return (
+        <>
+          <Head>
+            <title>User Requests</title>
+          </Head>
+          <h1 className="text-3xl font-bold text-center my-10">User Requests</h1>
+        </>
+    );
+}
---
+++ src/deskstar-frontend/pages/usersOverview.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function UsersOverview() {
+    return (
+        <>
+          <Head>
+            <title>Users Overview</title>
+          </Head>
+          <h1 className="text-3xl font-bold text-center my-10">Users Overview</h1>
+        </>
+    );
+}
