afa5703da11868672f6a0e43ec615ff145c1f613
eddyhakimi <eddi.hakimi@gmx.de>
2022-11-28 15:51:03 +0000

    Add user table
    Signed-off-by: eddyhakimi <eddi.hakimi@gmx.de>

--- src/deskstar-frontend/pages/userRequests.tsx
+++ src/deskstar-frontend/pages/userRequests.tsx
@@ -1,12 +1,37 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { UsersTable } from "../components/UsersTable";
+import { IUser } from "../types/users";
+import { UserManagementWrapper } from "../components/UserManagementWrapper";
+//TODO: delete this when using backend data instead of mockup
+import { users } from "../users";
 
-export default function UserRequests() {
-    return (
-        <>
-          <Head>
-            <title>User Requests</title>
-          </Head>
-          <h1 className="text-3xl font-bold text-center my-10">User Requests</h1>
-        </>
-    );
+export default function UserRequests({ users }: { users: IUser[] }) {
+  const onApprovalUpdate = async (
+    user: IUser,
+    decision: boolean
+  ): Promise<void> => {
+    //TODO: Implement
+    if (decision) console.log(`Approving user ${user.userId}...`);
+    else console.log(`Rejecting user ${user.userId}...`);
+  };
+
+  return (
+    <UserManagementWrapper>
+      <Head>
+        <title>User Requests</title>
+      </Head>
+      <h1 className="text-3xl font-bold text-center my-10">User Requests</h1>
+      <UsersTable users={users} onApprovalUpdate={onApprovalUpdate} />
+    </UserManagementWrapper>
+  );
 }
+
+//TODO: delete this when using backend data instead of mockup
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      users: users.filter((user: IUser) => !user.isApproved),
+    },
+  };
+};
--- src/deskstar-frontend/pages/usersOverview.tsx
+++ src/deskstar-frontend/pages/usersOverview.tsx
@@ -1,12 +1,49 @@
 import Head from "next/head";
+import { GetServerSideProps } from "next";
+import { UsersTable } from "../components/UsersTable";
+import { IUser } from "../types/users";
+import { UserManagementWrapper } from "../components/UserManagementWrapper";
+//TODO: delete this when using backend data instead of mockup
+import { users } from "../users";
 
-export default function UsersOverview() {
-    return (
-        <>
-          <Head>
-            <title>Users Overview</title>
-          </Head>
-          <h1 className="text-3xl font-bold text-center my-10">Users Overview</h1>
-        </>
-    );
+export default function UsersOverview({ users }: { users: IUser[] }) {
+  const onPermissionUpdate = async (user: IUser): Promise<void> => {
+    //TODO: Implement
+    if (user.isAdmin) console.log(`Demoting user ${user.userId}...`);
+    else console.log(`Promoting user ${user.userId}...`);
+  };
+
+  const onEdit = async (user: IUser): Promise<void> => {
+    //TODO: Implement
+    console.log(`Editing user ${user.userId}...`);
+  };
+
+  const onDelete = async (user: IUser): Promise<void> => {
+    //TODO: Implement
+    console.log(`Deleting user ${user.userId}...`);
+  };
+
+  return (
+    <UserManagementWrapper>
+      <Head>
+        <title>Users Overview</title>
+      </Head>
+      <h1 className="text-3xl font-bold text-center my-10">Users Overview</h1>
+      <UsersTable
+        users={users}
+        onPermissionUpdate={onPermissionUpdate}
+        onEdit={onEdit}
+        onDelete={onDelete}
+      />
+    </UserManagementWrapper>
+  );
 }
+
+//TODO: delete this when using backend data instead of mockup
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      users: users.filter((user: IUser) => user.isApproved),
+    },
+  };
+};
