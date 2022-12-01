33065bff1bd724a3b14148d623aee5b10e610f7d
Philipp Wörndle <philipp11198@gmail.com>
2022-12-01 03:02:03 +0100

    chore: added swagger documentation \n Co-authored-by: Stefan Pfahler <stefanpfahler@gmx.de>
    Signed-off-by: Philipp Wörndle <philipp11198@gmail.com>

--- src/deskstar-backend/Deskstar/Controllers/UserController.cs
+++ src/deskstar-backend/Deskstar/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using Deskstar.Models;
 using Deskstar.Usecases;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
 
@@ -21,7 +22,24 @@ public class UserController : ControllerBase
         _userUsecases = userUsecases;
     }
 
+    /// <summary>
+    /// Returns user specific information
+    /// </summary>
+    /// <returns> User information in JSON Format </returns>
+    /// <remarks>
+    /// Sample request:
+    ///     Get /bookings/range?n=100&skip=50&direction=DESC&from=1669021730904&end=1669121730904 with JWT Token
+    /// </remarks>
+    /// 
+    /// <response code="200">Returns the booking list</response>
+    /// <response code="500">Internal Server Error</response>
+    /// <response code="400">Bad Request</response>
     [HttpGet("me")]
+    [Authorize]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [Produces("application/json")]
     public IActionResult GetMe()
     {
 
