1042e2a821c90a1bc503c660976c217365367a10
xilef45 <88111260+xilef45@users.noreply.github.com>
2023-01-09 12:18:13 +0100

    fix git attributes

--- src/deskstar-frontend/pages/users/index.tsx
+++ src/deskstar-frontend/pages/users/index.tsx
@@ -77,7 +77,7 @@ export default function UsersOverview({ users }: { users: IUser[] }) {
               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
             />
           </svg>
-          <span>Warning: This can't be undone!</span>
+          <span>Warning: This can&apos;t be undone!</span>
           <div className="flex-none">
             <button className="btn btn-sm btn-ghost" onClick={() => doDelete}>
               DELETE
