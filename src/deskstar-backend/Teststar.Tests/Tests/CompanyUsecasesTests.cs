9fa7179b51cfa7e17b0f60df6e9fcf6f9df5234b
Jan Tiegges <jan.tiegges@campus.tu-berlin.de>
2023-01-23 11:07:09 +0000

    add CompanyUsecases Tests
    Signed-off-by: Jan Tiegges <jan.tiegges@campus.tu-berlin.de>

---
+++ src/deskstar-backend/Teststar.Tests/Tests/CompanyUsecasesTests.cs
@@ -0,0 +1,48 @@
+using Deskstar.Core.Exceptions;
+using Deskstar.DataAccess;
+using Deskstar.Entities;
+using Deskstar.Usecases;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Teststar.Tests.Tests;
+
+public class CompanyUsecasesTests
+{
+  private void setupMockData(DataContext db, Guid companyID, String companyName)
+  {
+    var company = new Company
+    {
+      CompanyId = companyID,
+      CompanyName = companyName
+    };
+    db.Companies.Add(company);
+    db.SaveChanges();
+  }
+
+  private void GetCompanies_ShouldReturnAllCompanies()
+  {
+    // setup
+    using var db = new DataContext();
+    var companyID = Guid.NewGuid();
+    var companyName = "Test Company";
+    setupMockData(db, companyID, companyName);
+
+    // arrange
+    var logger = new Mock<ILogger<CompanyUsecases>>();
+    var companyUsecases = new CompanyUsecases(db, logger.Object);
+
+
+    // act
+    var companies = companyUsecases.GetCompanies();
+
+    // assert
+    Assert.That(1 == companies.Count());
+    Assert.That(companyID == companies.First().CompanyId);
+    Assert.That(companyName == companies.First().CompanyName);
+
+    // cleanup
+    db.Database.EnsureDeleted();
+  }
+}
