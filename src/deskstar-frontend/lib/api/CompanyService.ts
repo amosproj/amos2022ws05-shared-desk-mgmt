e2bc2f80313f19ce8e0bdddfa9d78156920542f2
eddyhakimi <eddi.hakimi@gmx.de>
2023-01-23 11:34:05 +0100

    Add company service
    Signed-off-by: eddyhakimi <eddi.hakimi@gmx.de>

---
+++ src/deskstar-frontend/lib/api/CompanyService.ts
@@ -0,0 +1,18 @@
+import { BACKEND_URL } from "./constants";
+
+/**
+ * Returns list of registered companies
+ * @returns The list of companies
+ * @throws Error containing status code and/or error message
+ */
+async function getCompanies() {
+  const response = await fetch(`${BACKEND_URL}/companies`);
+
+  if (!response.ok) throw Error(`${response.status} ${response.statusText}`);
+
+  return response.json();
+}
+
+module.exports = {
+  getCompanies,
+};
