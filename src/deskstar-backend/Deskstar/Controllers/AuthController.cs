f67fd56b1d3afcf21334aa88cb7891793ff880f7
Philipp Wörndle <philipp11198@gmail.com>
2023-01-30 16:20:47 +0100

    feat: setup register admin endpoint
    Co-authored-by: Stefan Pfahler <stefanpfahler@gmx.de>
    Signed-off-by: Philipp Wörndle <philipp11198@gmail.com>

--- src/deskstar-backend/Deskstar/Controllers/AuthController.cs
+++ src/deskstar-backend/Deskstar/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using Deskstar.Entities;
 using Deskstar.Models;
 using Deskstar.Usecases;
 using Microsoft.AspNetCore.Authorization;
@@ -10,68 +11,92 @@ namespace Deskstar.Controllers;
 [Produces("text/plain")]
 public class AuthController : ControllerBase
 {
-    private readonly ILogger<AuthController> _logger;
-    private readonly IAuthUsecases _authUsecases;
-    private readonly IConfiguration _configuration;
+  private readonly ILogger<AuthController> _logger;
+  private readonly IAuthUsecases _authUsecases;
+  private readonly IConfiguration _configuration;
+
+  private readonly IAutoMapperConfiguration _autoMapperConfiguration;
 
 
-    public AuthController(ILogger<AuthController> logger, IAuthUsecases authUsecases, IConfiguration configuration)
+  public AuthController(ILogger<AuthController> logger, IAuthUsecases authUsecases, IConfiguration configuration, IAutoMapperConfiguration autoMapperConfiguration)
+  {
+    _logger = logger;
+    _authUsecases = authUsecases;
+    _configuration = configuration;
+    _autoMapperConfiguration = autoMapperConfiguration;
+  }
+
+  /// <summary>
+  /// Login functionality
+  /// </summary>
+  /// <returns> JWT, if users is approved and psw is correct </returns>
+  /// <remarks>
+  /// Sample request:
+  ///     Post /auth/createToken
+  /// </remarks>
+  /// 
+  /// <response code="200">Login succesful </response>
+  /// <response code="401">Credentials wrong or user not approved</response>
+  [HttpPost("createToken")]
+  [AllowAnonymous]
+  [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+  [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status401Unauthorized)]
+  public IActionResult CreateToken(CreateTokenUser user)
+  {
+    var returnValue = _authUsecases.CheckCredentials(user.MailAddress, user.Password);
+    if (returnValue.Message == LoginReturn.Ok)
     {
-        _logger = logger;
-        _authUsecases = authUsecases;
-        _configuration = configuration;
+      return Ok(_authUsecases.CreateToken(_configuration, user.MailAddress));
     }
 
-    /// <summary>
-    /// Login functionality
-    /// </summary>
-    /// <returns> JWT, if users is approved and psw is correct </returns>
-    /// <remarks>
-    /// Sample request:
-    ///     Post /auth/createToken
-    /// </remarks>
-    /// 
-    /// <response code="200">Login succesful </response>
-    /// <response code="401">Credentials wrong or user not approved</response>
-    [HttpPost("createToken")]
-    [AllowAnonymous]
-    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status401Unauthorized)]
-    public IActionResult CreateToken(CreateTokenUser user)
-    {
-        var returnValue = _authUsecases.CheckCredentials(user.MailAddress, user.Password);
-        if (returnValue.Message == LoginReturn.Ok)
-        {
-            return Ok(_authUsecases.CreateToken(_configuration, user.MailAddress));
-        }
+    return Unauthorized(returnValue.Message.ToString());
+  }
 
-        return Unauthorized(returnValue.Message.ToString());
-    }
-
-    /// <summary>
-    /// Register functionality
-    /// </summary>
-    /// <remarks>
-    /// Sample request:
-    ///     Post /auth/register
-    /// </remarks>
-    /// 
-    /// <response code="200">User added to db</response>
-    /// <response code="400">Mail already in use</response>
-    /// <response code="404">Company not found</response>
-    [HttpPost("register")]
-    [AllowAnonymous]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(RegisterResponse), StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(RegisterResponse), StatusCodes.Status404NotFound)]
-    public IActionResult Register(RegisterUser registerUser)
+  /// <summary>
+  /// Register functionality
+  /// </summary>
+  /// <remarks>
+  /// Sample request:
+  ///     Post /auth/register
+  /// </remarks>
+  /// 
+  /// <response code="200">User added to db</response>
+  /// <response code="400">Mail already in use</response>
+  /// <response code="404">Company not found</response>
+  [HttpPost("register")]
+  [AllowAnonymous]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(typeof(RegisterResponse), StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(typeof(RegisterResponse), StatusCodes.Status404NotFound)]
+  public IActionResult Register(RegisterUser registerUser)
+  {
+    var result = _authUsecases.RegisterUser(registerUser);
+    return result.Message switch
     {
-        var result = _authUsecases.RegisterUser(registerUser);
-        return result.Message switch
-        {
-            RegisterReturn.Ok => Ok(),
-            RegisterReturn.CompanyNotFound => NotFound(result.Message.ToString()),
-            _ => BadRequest(result.Message.ToString())
-        };
-    }
+      RegisterReturn.Ok => Ok(),
+      RegisterReturn.CompanyNotFound => NotFound(result.Message.ToString()),
+      _ => BadRequest(result.Message.ToString())
+    };
+  }
+  /// <summary>
+  /// Register functionality
+  /// </summary>
+  /// <remarks>
+  /// Sample request:
+  ///     Post /auth/registerAdmin
+  /// </remarks>
+  /// 
+  /// <response code="200">Admin added to db</response>
+  /// <response code="400">Mail or Company name already in use</response>
+  [HttpPost("registerAdmin")]
+  [AllowAnonymous]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(typeof(RegisterResponse), StatusCodes.Status400BadRequest)]
+  public IActionResult RegisterAdmin(RegisterAdminDto registerUser)
+  {
+    var admin = _authUsecases.RegisterAdmin(registerUser);
+    var mapper = _autoMapperConfiguration.GetConfiguration().CreateMapper();
+    var dto = mapper.Map<User, RegisterAdminResponseObject>(admin);
+    return Ok(dto);
+  }
 }
