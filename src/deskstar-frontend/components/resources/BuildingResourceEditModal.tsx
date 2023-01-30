86b1bc0df4664637d0ffa4bc44b006281c91a952
Philipp Wörndle <philipp11198@gmail.com>
2023-01-30 11:24:19 +0100

    feat: create edit building modal component
    Co-authored-by: Stefan Pfahler <stefanpfahler@gmx.de>
    Signed-off-by: Philipp Wörndle <philipp11198@gmail.com>

--- src/deskstar-frontend/components/resources/BuildingResourceEditModal.tsx
+++ src/deskstar-frontend/components/resources/BuildingResourceEditModal.tsx
@@ -1,20 +1,74 @@
-import React from "react";
+import { Session } from "next-auth";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { updateBuilding } from "../../lib/api/ResourceService";
 import { IBuilding } from "../../types/building";
 import { ILocation } from "../../types/location";
+import Input from "../forms/Input";
 
 const BuildingResourceEditModal = ({
     building,
-    locations
+    isOpen,
+    setState,
+    updateBuilding,
+    buildingName,
+    setBuildingName,
+    location,
+    setLocation
 }: {
-    building:IBuilding;
-    locations:ILocation[];
+    building: IBuilding | undefined;
+    isOpen: boolean;
+    setState: Function;
+    updateBuilding: Function;
+    setBuildingName: Function;
+    setLocation: Function;
+    buildingName: string;
+    location: string;
 }) => {
+    if (!building) return (<></>)
     return (
         <>
-            <input type="checkbox" id={building.buildingId} className="modal-toggle" />
-            <div className="modal">
+            <div className={isOpen ? "modal modal-open" : "modal"}>
                 <div className="modal-box">
-                    {building.buildingName}
+                    <a className="btn btn-sm btn-circle float-right" onClick={() => { setState(false); }}>
+                        x
+                    </a>
+                    <h1 className="text-2xl pb-4">Edit Building</h1>
+                    <Input
+                        name="Change Building Name"
+                        onChange={(e) => {
+                            setBuildingName(e.target.value);
+                            console.log(e.target.value);
+                        }}
+                        value={buildingName}
+                        placeholder={building.buildingName}
+                    />
+                    <Input
+                        name="Change Location"
+                        onChange={(e) => {
+                            setLocation(e.target.value);
+                            console.log(e.target.value);
+                        }}
+                        value={location}
+                        placeholder={building.location}
+                    />
+                    <div className="flex justify-end">
+                        <button
+                            className="btn bg-deskstar-green-dark text-black hover:bg-deskstar-green-light"
+                            onClick={() => {
+                                //check changes
+                                if (buildingName === '') {
+                                    toast.error("Building name must not be empty");
+                                    return;
+                                }
+                                const changeBuildingName = building.buildingName === buildingName ? undefined : buildingName;
+                                const changeLocation = building.location === location ? undefined : location;
+                                const updateBuildingDto = { buildingName: changeBuildingName, location: changeLocation, };
+                                updateBuilding(updateBuildingDto, building);
+                            }}
+                        >Confirm</button>
+                    </div>
                 </div>
             </div>
         </>
