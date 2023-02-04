d4f5076d322b53361da08908a3f8e5641a895a23
jantiegges <52353665+jantiegges@users.noreply.github.com>
2023-02-04 15:21:34 +0100

    clean import
    Signed-off-by: jantiegges <52353665+jantiegges@users.noreply.github.com>

--- src/deskstar-frontend/pages/login.tsx
+++ src/deskstar-frontend/pages/login.tsx
@@ -4,7 +4,6 @@ import Head from "next/head";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import LoginPanel from "../components/LoginPanel";
-import { toast } from "react-toastify";
 
 export default function Login() {
   const { data: session } = useSession();
