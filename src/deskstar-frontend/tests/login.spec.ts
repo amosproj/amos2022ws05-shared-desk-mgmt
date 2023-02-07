cf972bc0f9cbc3d08211a74adac595aa9cfc475d
eddyhakimi <eddi.hakimi@gmx.de>
2023-02-07 12:17:35 +0100

    Add basic login tests
    Signed-off-by: eddyhakimi <eddi.hakimi@gmx.de>

---
+++ src/deskstar-frontend/tests/login.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from "@playwright/test";
+
+test("login with registered admin email and correct password", async ({
+  page,
+}) => {
+  await page.goto("/login");
+
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("alice.admin@acme.com");
+  await page.getByPlaceholder("Email").press("Tab");
+  await page.getByPlaceholder("Password").fill("test123");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // Displays welcome page
+  await expect(page).toHaveURL("/");
+  await expect(
+    page.getByRole("heading", {
+      name: "Hello Alice Admin, welcome back to Deskstar",
+    })
+  ).toHaveCount(1);
+});
+
+test("login with registered employee email and correct password", async ({
+  page,
+}) => {
+  await page.goto("/login");
+
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("bob.employee@acme.com");
+  await page.getByPlaceholder("Email").press("Tab");
+  await page.getByPlaceholder("Password").fill("test123");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // Displays welcome page
+  await expect(page).toHaveURL("/");
+  await expect(
+    page.getByRole("heading", {
+      name: "Hello Bob Employee, welcome back to Deskstar",
+    })
+  ).toHaveCount(1);
+});
+
+test("login with unknown email and password fails", async ({ page }) => {
+  await page.goto("/login");
+
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("trudy@unknown.com");
+  await page.getByPlaceholder("Email").press("Tab");
+  await page.getByPlaceholder("Password").fill("test123");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // Displays welcome page
+  await expect(page).toHaveURL("/login");
+  await expect(page.getByText("ErrorInvalidCredentials")).toHaveCount(1);
+});
